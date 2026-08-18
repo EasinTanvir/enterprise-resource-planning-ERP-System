06f0c7bfd36459a9356ed9000124065dbf5a5077
EasinTanvir <easintanvir102@gmail.com>
2026-08-18 22:36:29 +0600

    implement basic configuration to backend

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import morgan from 'morgan';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(morgan('dev'));
+
+  const PORT = process.env.PORT;
+  await app.listen(PORT ?? 3000, () =>
+    console.log(`server running on ${PORT}`),
+  );
 }
 bootstrap();
