75e51c483f549a5dce2fbf633dfbcab183ec0604
EasinTanvir <easintanvir102@gmail.com>
2026-08-18 22:37:49 +0600

    refactor

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [AuthenticationModule, ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
