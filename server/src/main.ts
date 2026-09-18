4d4b743635344a8f1a253eb364759aa4d101822b
EasinTanvir <easintanvir102@gmail.com>
2026-09-18 23:21:11 +0600

    refactor

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import morgan from 'morgan';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(morgan('dev'));
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
